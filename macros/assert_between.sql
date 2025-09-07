c411b6f9349ba968dd5bc227eecd4d5e324c94ce
Nanda Govindan <nanda.gnk@gmail.com>
2025-09-07 04:23:24 +0000

    DBT Test  Custom generic test and singular test

---
+++ macros/assert_between.sql
@@ -0,0 +1,8 @@
+{% test assert_between(model, column_name, min_value, max_value) %}
+
+    SELECT *
+    FROM {{ model }}
+    WHERE {{ column_name }} < {{ min_value }}
+       OR {{ column_name }} > {{ max_value }}
+
+{% endtest %}
